b8aaf3ec474b70ce91b90dbf5f0297348929719c
charuharan <chari301431@gmail.com>
2021-07-29 18:24:37 +0530

    encap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoEncapsulationComponent } from './no-encapsulation/no-encapsulation.component';
+import { EmulatedEncapsulationComponent } from './emulated-encapsulation/emulated-encapsulation.component';
+import { ShadowDomEncapsulationComponent } from './shadow-dom-encapsulation/shadow-dom-encapsulation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoEncapsulationComponent,
+    EmulatedEncapsulationComponent,
+    ShadowDomEncapsulationComponent
   ],
   imports: [
     BrowserModule,
